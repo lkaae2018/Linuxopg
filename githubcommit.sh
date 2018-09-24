d37886397df1fe1ebfa8215cb11f28a10409ebb8
lkaae2018 <lak@djhhadsten.dk>
2018-09-24 12:17:05 +0200

    ADCupdate2

--- githubcommit.sh
+++ githubcommit.sh
@@ -4,7 +4,7 @@
 git add *
 git config --global user.email "lak@djhhadsten.dk"
 git config --global user.name "lkaae2018"
-echo Navn til  commiten?
+echo Navn til commiten?
 read commit
-git commit -m commit
+git commit -m $commit
 git push
